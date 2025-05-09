3e32f618076dee8a328ec67b7a3d805ca901b9fc
rs691 <robstewart@my365.bellevue.edu>
2025-05-09 09:42:12 -0500

    updated src

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
   site: 'https://rstewart.me/',
-  base: '/astro-react',
-  integrations: [react()]
+  base: '/astro-tailwinds',
+  integrations: [react(), mdx(), sitemap()]
 });
